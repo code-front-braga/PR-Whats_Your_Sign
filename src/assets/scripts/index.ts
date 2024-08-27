ab22ebf2876eeddb57cbd56c433b2ddd43a7f7a8
Leonardo Braga <programer_frontbraga@outlook.com>
2024-08-26 22:59:45 -0300

    chore: Anotações sobre as funções que devem ser criadas.

---
+++ src/assets/scripts/index.ts
@@ -0,0 +1,4 @@
+// Criar função para captar o nome do usuário;
+// Criar função para captar a data de nascimento do usuário (verificar REGEX);
+// Criar função para alterar as imagens dos signos, conforme data de nascimento do usuário;
+// Criar função que gera o texto Ex.: "Seu signo é: Touro";
