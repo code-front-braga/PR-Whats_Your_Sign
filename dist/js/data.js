46997c629ecb6672159dd9c80d3012f35274ffae
Leonardo Braga <programer_frontbraga@outlook.com>
2024-09-01 00:16:00 -0300

    test: Formato das imagens alteradas para jpg

--- dist/js/data.js
+++ dist/js/data.js
@@ -1,16 +1,16 @@
 export const zodiacImages = {
-    ariesImage: 'assets/images/aries.png',
-    taurusImage: 'assets/images/taurus.png',
-    geminiImage: 'assets/images/gemini.png',
-    cancerImage: 'assets/images/cancer.png',
-    leoImage: 'assets/images/leo.png',
-    virgoImage: 'assets/images/virgo.png',
-    libraImage: 'assets/images/libra.png',
-    scorpioImage: 'assets/images/scorpio.png',
-    sagittariusImage: 'assets/images/sagittarius.png',
-    capricornImage: 'assets/images/carpicorn.png',
-    aquariusImage: 'assets/images/aquarius.png',
-    piscesImage: 'assets/images/pisces.png',
+    ariesImage: 'assets/images/aries.jpg',
+    taurusImage: 'assets/images/taurus.jpg',
+    geminiImage: 'assets/images/gemini.jpg',
+    cancerImage: 'assets/images/cancer.jpg',
+    leoImage: 'assets/images/leo.jpg',
+    virgoImage: 'assets/images/virgo.jpg',
+    libraImage: 'assets/images/libra.jpg',
+    scorpioImage: 'assets/images/scorpio.jpg',
+    sagittariusImage: 'assets/images/sagittarius.jpg',
+    capricornImage: 'assets/images/carpicorn.jpg',
+    aquariusImage: 'assets/images/aquarius.jpg',
+    piscesImage: 'assets/images/pisces.jpg',
 };
 export function getZodiacSign(inputBirthday) {
     const [day, month] = inputBirthday.value.split('/').map(Number);
